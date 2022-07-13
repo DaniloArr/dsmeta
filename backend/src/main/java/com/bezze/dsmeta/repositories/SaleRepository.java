387bf9d667332ea8b7fd05843a0131fad7cb71c2
DaniloArr <danilo.arreb@gmail.com>
2022-07-13 17:26:25 -0300

    API Test

---
+++ backend/src/main/java/com/bezze/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.bezze.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bezze.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
